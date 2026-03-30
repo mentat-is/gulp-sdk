4bea20efd70328ee668f0194aa3e58bbfadb9ed3
valerino <xoanino@gmail.com>
2026-03-30 13:46:53 +0000

    fixed version string

--- src/gulp_sdk/__init__.py
+++ src/gulp_sdk/__init__.py
@@ -23,6 +23,18 @@ Quick start:
             print(doc.id, doc.content[:50])
     ```
 """
+try:
+    from ._version import version as __version__, commit_id
+except ImportError:
+    __version__ = "0.0.0"
+    commit_id = None
+
+if commit_id:
+    __version_full__ = f"{__version__}+{commit_id}"
+else:
+    __version_full__ = __version__
+
+__all__ = ["__version__", "__version_full__"]
 
 from gulp_sdk.client import GulpClient
 from gulp_sdk.exceptions import (
